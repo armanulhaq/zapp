6b021d0faea913d33f6a0961615fc4337ed00aaa
Arman Ul Haq <armanulhaq10@gmail.com>
2025-06-03 15:27:17 +0530

    add login logout state logic to navbar

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -7,8 +7,18 @@ export const AppContextProvider = ({ children }) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+
     //Step3: put all the states that are needed in a single object
-    const value = { navigate, user, setUser, setIsSeller, isSeller };
+    const value = {
+        navigate,
+        user,
+        setUser,
+        setIsSeller,
+        isSeller,
+        showUserLogin,
+        setShowUserLogin,
+    };
     //. Step4: Provide the context value to the children {app.jsx}
     return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
