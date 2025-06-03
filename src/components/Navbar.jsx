c07c1ebed2b1a8b550c7354c32acda5efa0e8665
Arman Ul Haq <armanulhaq10@gmail.com>
2025-06-03 13:55:08 +0530

    add mobile responsive navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import "../index.css";
+import { NavLink } from "react-router-dom";
+
+export const Navbar = () => {
+    const [open, setOpen] = useState(false);
+    return (
+        <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-100 bg-white relative transition-all">
+            <NavLink to="/" className="flex justify-center items-center gap-2">
+                <img className="h-9" src="/brand.png" alt="Zapp Logo" />
+                <div className="text-yellow-400 font-extrabold text-3xl">
+                    Zapp
+                </div>
+            </NavLink>
+
+            {/* Desktop Menu */}
+            <div className="hidden sm:flex items-center gap-8">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/products">All Products</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+
+                <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-100  bg-gray-50 pr-5 rounded-xl">
+                    <input
+                        className="px-5 py-3 w-full outline-none placeholder-gray-300 rounded-xl"
+                        type="text"
+                        placeholder="Search products"
+                    />
+                    <svg
+                        width="16"
+                        height="16"
+                        viewBox="0 0 16 16"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M10.836 10.615 15 14.695"
+                            stroke="#7A7B7D"
+                            stroke-width="1.2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                        <path
+                            clip-rule="evenodd"
+                            d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783"
+                            stroke="#7A7B7D"
+                            stroke-width="1.2"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                </div>
+
+                <div className="relative cursor-pointer">
+                    <svg
+                        width="18"
+                        height="18"
+                        viewBox="0 0 14 14"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0"
+                            stroke="#000000"
+                            stroke-linecap="round"
+                            stroke-linejoin="round"
+                        />
+                    </svg>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-[#ffd60a] w-[18px] h-[18px] rounded-full">
+                        3
+                    </button>
+                </div>
+
+                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white font-md text-sm rounded-xl">
+                    Login
+                </button>
+            </div>
+
+            <button
+                onClick={() => (open ? setOpen(false) : setOpen(true))}
+                aria-label="Menu"
+                className="sm:hidden"
+            >
+                {/* Menu Icon SVG */}
+                <svg
+                    width="21"
+                    height="15"
+                    viewBox="0 0 21 15"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <rect width="21" height="1.5" rx=".75" fill="#426287" />
+                    <rect
+                        x="8"
+                        y="6"
+                        width="13"
+                        height="1.5"
+                        rx=".75"
+                        fill="#426287"
+                    />
+                    <rect
+                        x="6"
+                        y="13"
+                        width="15"
+                        height="1.5"
+                        rx=".75"
+                        fill="#426287"
+                    />
+                </svg>
+            </button>
+
+            {/* Mobile Menu */}
+            <div
+                className={`${
+                    open ? "flex" : "hidden"
+                } absolute top-[60px] left-0 w-full bg-white shadow-sm py-4 flex-col items-center gap-4 px-5 text-sm md:hidden`}
+            >
+                <NavLink to="/" className="block">
+                    Home
+                </NavLink>
+                <NavLink href="/products" className="block">
+                    All Products
+                </NavLink>
+                <NavLink to="/contact" className="block">
+                    Contact
+                </NavLink>
+                <button className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-xl text-sm">
+                    Login
+                </button>
+            </div>
+        </nav>
+    );
+};
