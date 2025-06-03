2f9ba47cc5a9c4a4d94a3c88adb3ca4022be4618
Arman Ul Haq <armanulhaq10@gmail.com>
2025-06-03 13:06:43 +0530

    add context for easy state management

---
+++ src/context/AppContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext(); //1. Step1: Create a context
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    //Step3: put all the states that are needed in a single object
+    const value = { navigate, user, setUser, setIsSeller, isSeller };
+    //. Step4: Provide the context value to the children {app.jsx}
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+    // Custom hook to use the context easily in any child component
+    return useContext(AppContext);
+};
