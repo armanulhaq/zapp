10399e0a2a32c3a7cec37a28f1ee702181576f82
Arman Ul Haq <armanulhaq10@gmail.com>
2025-06-04 17:51:55 +0530

    Initial commit with client folder

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,84 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
+
+export const AppContext = createContext(); //1. Step1: Create a context
+
+export const AppContextProvider = ({ children }) => {
+    const currency = import.meta.VITE_CURRENCY;
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
+    const [products, setProducts] = useState([]);
+    const [cartItems, setCartItems] = useState({});
+
+    const fetchProducts = async () => {
+        setProducts(dummyProducts);
+    };
+
+    const addToCart = (itemId) => {
+        // Create a deep copy of the current cart items to avoid direct state mutation
+        let cartData = structuredClone(cartItems);
+        //Checking if a product is already in the cartData. If yes +1, if no 1.
+        if (cartData[itemId]) {
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to cart");
+    };
+
+    //Update cart item quantity
+    const updateCartItem = (itemId, quantity) => {
+        let cartData = structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart updated");
+    };
+
+    //remove product from cart
+    const removeFromCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+            cartData[itemId] -= 1;
+            if (cartData[itemId] === 0) {
+                delete cartData[itemId];
+            }
+        }
+
+        toast.success("Removed to cart");
+        setCartItems(cartData);
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    //Step3: put all the states that are needed in a single object
+    const value = {
+        navigate,
+        user,
+        setUser,
+        setIsSeller,
+        isSeller,
+        showUserLogin,
+        setShowUserLogin,
+        products,
+        currency,
+        addToCart,
+        updateCartItem,
+        removeFromCart,
+        cartItems,
+    };
+    //. Step4: Provide the context value to the children {app.jsx}
+    return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+    // Custom hook to use the context easily in any child component
+    return useContext(AppContext);
+};
