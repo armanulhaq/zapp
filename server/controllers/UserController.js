aa1d073dc31e7451db7b6fcebb269398dcc7e655
Arman Ul Haq <armanulhaq10@gmail.com>
2025-06-07 17:46:49 +0530

    add login endpoint functionality

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -48,4 +48,43 @@ export const register = async (req, res) => {
     }
 };
 
-export default register;
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.json({
+                success: false,
+                message: "Please fill all the details.",
+            });
+        }
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.json({
+                success: false,
+                message: "Invalid email or password",
+            });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.json({ success: false, message: "Invalid credentials" });
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        return res.json({
+            success: true,
+            user: { email: user.email, name: user.name },
+            message: "User successfully created",
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+};
